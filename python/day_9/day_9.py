c962e364d5654282e5dee64add62d8b4d12c2224
asrospie <asrospierski@gmail.com>
2024-12-09 12:23:12 -0500

    working on part 2

--- python/day_9/day_9.py
+++ python/day_9/day_9.py
@@ -43,25 +43,49 @@ def day_9_part_1(filename):
 
 
 def day_9_part_2(filename):
-    ls = parse_input(filename)
+    ls = []
+    with open(filename, 'r') as f:
+        count = 0
+        is_file = True
+        for c in f.readline().strip():
+            if is_file:
+                ls.append([count, int(c)])
+                count += 1
+            else:
+                ls.append([-1, int(c)])
+            is_file = not is_file
 
-    for i, c in enumerate(ls):
-        if c != '.':
+    print(ls)
+
+    i = 0
+    while i < len(ls):
+        if ls[i][0] != -1:
+            i += 1
             continue
 
-        # get idxs of '.'
-        idxs, cnt = [], 0
-        while c[cnt + i] == '.':
-            idxs.append(i + cnt)
-            cnt += 1
+        swap_idx = -1
+        # find fitting file
+        for j, v in enumerate(ls[::-1]):
+            if v[0] != -1 and v[1] <= ls[i][1]:
+                swap_idx = len(ls) - 1 - j
+                break
+        if swap_idx == -1:
+            i += 1
+            continue
 
-        # find file that can fit
+        # swap values
+        diff = ls[i][1] - ls[swap_idx][1]
+        print(f'{ls[i]} :: {ls[swap_idx]}')
+        ls[i] = ls[swap_idx]
+        ls[swap_idx][0] = -1
+        if diff != 0:
+            ls.insert(i + 1, [-1, diff])
+            i += 1
+        i += 1
+
+    print(ls)
+
     result = 0
-    for i, v in enumerate(ls):
-        if v == '.':
-            continue
-        result += i * int(v)
-
     return result
 
 
