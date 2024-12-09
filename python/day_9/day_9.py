dca23842d6750fdfad149cbf8946ba8dcb929aad
asrospie <asrospierski@gmail.com>
2024-12-09 12:47:50 -0500

    Finish day_9, used brute force but two different methods for each

--- python/day_9/day_9.py
+++ python/day_9/day_9.py
@@ -5,6 +5,16 @@ def find_last(lst):
     return -1
 
 
+def print_grid(lst):
+    for v in lst:
+        if v[0] == -1:
+            print('.' * v[1], end='')
+        else:
+            print(f'{str(v[0]) * v[1]}', end='')
+
+    print()
+
+
 def parse_input(filename):
     with open(filename, 'r') as f:
         s = f.readline().strip()
@@ -55,45 +65,48 @@ def day_9_part_2(filename):
                 ls.append([-1, int(c)])
             is_file = not is_file
 
-    print(ls)
 
-    i = 0
-    while i < len(ls):
-        if ls[i][0] != -1:
-            i += 1
+    i = len(ls) - 1
+    while i >= 0:
+        # skip blanks
+        if ls[i][0] == -1:
+            i -= 1
             continue
 
+        # find swappable space
         swap_idx = -1
-        # find fitting file
-        for j, v in enumerate(ls[::-1]):
-            if v[0] != -1 and v[1] <= ls[i][1]:
-                swap_idx = len(ls) - 1 - j
+        for j, v in enumerate(ls):
+            if j >= i:
+                break
+            if v[0] == -1 and v[1] >= ls[i][1]:
+                swap_idx = j
                 break
         if swap_idx == -1:
-            i += 1
+            i -= 1
             continue
 
-        # swap values
-        diff = ls[i][1] - ls[swap_idx][1]
-        print(f'{ls[i]} :: {ls[swap_idx]}')
-        ls[i] = ls[swap_idx]
-        ls[swap_idx][0] = -1
-        if diff != 0:
-            ls.insert(i + 1, [-1, diff])
-            i += 1
-        i += 1
+        diff = ls[swap_idx][1] - ls[i][1]
+        ls[swap_idx][0], ls[swap_idx][1] = ls[i][0], ls[i][1]
+        ls[i][0] = -1
+        if diff >= 0:
+            ls.insert(swap_idx + 1, [-1, diff])
 
-    print(ls)
+        i -= 1
 
     result = 0
+    count = 0
+    for v in ls:
+        for _ in range(v[1]):
+            result += count * (v[0] if v[0] != -1 else 0)
+            count += 1
     return result
 
 
 def main():
-    # print(day_9_part_1('day_9.test'))
-    # print(day_9_part_1('day_9.input'))
+    print(day_9_part_1('day_9.test'))
+    print(day_9_part_1('day_9.input'))
     print(day_9_part_2('day_9.test'))
-    # print(day_9_part_2('day_9.input'))
+    print(day_9_part_2('day_9.input'))
 
 if __name__ == '__main__':
     main()
