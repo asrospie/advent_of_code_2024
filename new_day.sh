ca76b3029cdcf41dd160e3501506205b73dbcfb7
asrospie <asrospierski@gmail.com>
2024-12-11 16:11:42 -0500

    update script

--- new_day.sh
+++ new_day.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
 day_of_month=$(date +"%d")
+lang="python"
 
-if [ "$1" == "python" ]; then
-  if [ ! -d "python" ]; then
-    mkdir python
-  fi
-
-  if [ ! -d "python/day_$day_of_month" ]; then
-    mkdir -p "python/day_$day_of_month"
-  fi
-
-  cp python/day_template.py "python/day_$day_of_month/day_$day_of_month.py"
-  sed -i '' "s/num/$day_of_month/g" "python/day_$day_of_month/day_$day_of_month.py"
-  touch "python/day_$day_of_month/day_$day_of_month.test"
-  touch "python/day_$day_of_month/day_$day_of_month.input"
+if [ ! -z $1 ]; then
+  lang=$1
 fi
+
+if [ ! -z $2 ]; then
+  day_of_month=$2
+fi
+
+parent_dir=$lang
+day_dir="$parent_dir/day_$day_of_month"
+file_path="$day_dir/day_$day_of_month"
+
+if [ ! -d $lang ]; then
+  mkdir $lang
+fi
+
+if [ ! -d "$lang/day_$day_of_month" ]; then
+  mkdir -p "$day_dir"
+fi
+
+if [ $lang == "python" ]; then
+  echo "Creating $file_path.py from $lang/day_template.py..."
+  cp $lang/day_template.py "$file_path.py"
+  sed -i '' "s/num/$day_of_month/g" "$lang/day_$day_of_month/day_$day_of_month.py"
+elif [ $lang == "raku" ]; then
+  echo "Creating $file_path.raku"
+  touch "$file_path.raku"
+fi
+
+echo "Creating $file_path.test & $file_path.input..."
+touch "$file_path.test"
+touch "$file_path.input"
+
+echo
+echo "--- Output ---"
+ls $day_dir
